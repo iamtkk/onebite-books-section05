471d387ba5db8becdc637eb2b379807048ebd8d2
김태관 <iamtk@neowiz.com>
2024-11-05 20:37:53 +0900

    first commit

---
+++ src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,26 @@
+import BookItem from '@/components/book-item';
+import { BookData } from '@/types';
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    q?: string;
+  }>;
+}) {
+  const { q } = await searchParams;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`
+  );
+  if (!response.ok) {
+    return <div>Failed to fetch books</div>;
+  }
+  const books: BookData[] = await response.json();
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
