2810952f81b3d5f7b41410b4ccec64695b4b35fb
김태관 <iamtk@neowiz.com>
2024-11-06 00:12:24 +0900

    client cache

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,5 +1,12 @@
-import { BookData } from '@/types';
-import style from './page.module.css';
+import { BookData } from "@/types";
+import style from "./page.module.css";
+import { notFound } from "next/navigation";
+
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return [{ id: "1" }, { id: "2" }, { id: "3" }];
+}
 
 export default async function Page({
   params,
@@ -11,6 +18,9 @@ export default async function Page({
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${id}`
   );
   if (!response.ok) {
+    if (response.status === 404) {
+      notFound();
+    }
     return <div>Failed to fetch book</div>;
   }
   const book: BookData = await response.json();
